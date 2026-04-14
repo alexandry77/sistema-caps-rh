c0a04cbb11f76bee8eebba4ec150fc7dc4175e28
Alexandry Tavares <102990838+alexandry77@users.noreply.github.com>
2026-04-14 19:25:46 -0300

    Add files via upload

---
+++ java/com/caps/rh/Funcionario.java
@@ -0,0 +1,32 @@
+package com.caps.rh;
+
+public class Funcionario {
+
+    private String nome;
+    private String cpf;
+    private double salario;
+
+    public Funcionario(String nome, String cpf, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void aplicarAumento(double percentual) {
+        if (percentual > 0) {
+            salario += salario * (percentual / 100);
+        }
+    }
+}
